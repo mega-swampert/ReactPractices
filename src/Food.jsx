65954bf7a36734e02a8c8856073a6ea36be49aba
huanmiaoi <47890849+huanmiaoi@users.noreply.github.com>
2024-09-04 14:22:56 +1000

    Initial commit

---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+function Food() {
+
+  const food1 = "orange";
+  const food2 = "apple";
+  return (
+    <ul>
+      <li>Banana</li>
+      <li>{food1}</li>
+      <li>{food2.toUpperCase()}</li>
+    </ul>
+
+  );
+
+}
+
+export default Food;
